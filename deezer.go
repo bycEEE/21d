6ee97c2c37df124f5eb33cf975bfb40ab41aa39f
Brian Choy <bycEEE@gmail.com>
2018-10-29 14:32:24 -0400

    Create API client, restructure default headers and query values

--- deezer.go
+++ deezer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -26,45 +27,14 @@ func SetDefaultHeaders(r *http.Request) *http.Request {
 	return r
 }
 
-// NewDefaultPrivateAPIURL returns a URL type containing default parameters the
-// Deezer Private API accepts. This is used to quickly set parameters
-// such as `api_token` and `method`.
-func NewDefaultPrivateAPIURL() (u *url.URL, err error) {
-	u, err = url.Parse(privateAPIURL)
-	if err != nil {
-		return nil, err
-	}
-	q := u.Query()
-	q.Set("api_version", "1.0")
-	q.Set("api_token", "null")
-	q.Set("input", "3")
-	// q.Set("method", "null")
-	u.RawQuery = q.Encode()
-	return u, nil
-}
-
 // GetCheckFormLogin retrieves the checkFormLogin parameter that gets sent to
 // https://www.deezer.com/ajax/action.php when logging in. This is used for
 // enabling auto-logins.
-func (c *PrivateClient) GetCheckFormLogin() (l string, err error) {
-	// Create URL string
-	u, err := NewDefaultPrivateAPIURL()
-	if err != nil {
-		return "", err
-	}
-	q := u.Query()
-	q.Set("method", "deezer.getUserData")
-	u.RawQuery = q.Encode()
-
-	// Create request and add headers
-	req, err := http.NewRequest("GET", u.String(), nil)
-	if err != nil {
-		return "", err
-	}
-	req = SetDefaultHeaders(req)
-
-	// Send request and read body
-	resp, err := c.httpClient.Do(req)
+func (c *PrivateClient) GetCheckFormLogin() (string, error) {
+	ctx := context.Background()
+	v := url.Values{}
+	v.Set("method", "deezer.getUserData")
+	resp, err := c.get(ctx, v, nil)
 	if err != nil {
 		return "", err
 	}
@@ -80,24 +50,52 @@ func (c *PrivateClient) GetCheckFormLogin() (l string, err error) {
 	if err != nil {
 		return "", err
 	}
+	// errors come in a map or empty array. Need to address inconsistent types
+	//if pb.Error.GatewayError != "" {
+	//	return "", fmt.Errorf(pb.Error.GatewayError)
+	//}
 	if pb.Results.CheckFormLogin == "" {
 		return "", fmt.Errorf("checkFormLogin value is empty")
 	}
 	return pb.Results.CheckFormLogin, nil
 }
 
-// Deezer.prototype.getToken = async function(){
-// 	const res = await request.get({
-// 		url: this.apiUrl,
-// 		headers: this.httpHeaders,
-// 		strictSSL: false,
-// 		qs: {
-// 			api_version: "1.0",
-// 			api_token: "null",
-// 			input: "3",
-// 			method: 'deezer.getUserData'
-// 		},
-// 		json: true,
-// 		jar: true,
-// 	})
-// 	return res.body.results.checkForm;
+//func (c *PrivateClient) GetCheckFormLogin() (l string, err error) {
+//	// Create URL string
+//	u, err := NewDefaultPrivateAPIURL()
+//	if err != nil {
+//		return "", err
+//	}
+//	q := u.Query()
+//	q.Set("method", "deezer.getUserData")
+//	u.RawQuery = q.Encode()
+//
+//	// Create request and add headers
+//	req, err := http.NewRequest("GET", u.String(), nil)
+//	if err != nil {
+//		return "", err
+//	}
+//	req = SetDefaultHeaders(req)
+//
+//	// Send request and read body
+//	resp, err := c.httpClient.Do(req)
+//	if err != nil {
+//		return "", err
+//	}
+//	defer resp.Body.Close()
+//	body, err := ioutil.ReadAll(resp.Body)
+//	if err != nil {
+//		return "", err
+//	}
+//
+//	// Unmarshal
+//	var pb PrivateBody
+//	err = json.Unmarshal(body, &pb)
+//	if err != nil {
+//		return "", err
+//	}
+//	if pb.Results.CheckFormLogin == "" {
+//		return "", fmt.Errorf("checkFormLogin value is empty")
+//	}
+//	return pb.Results.CheckFormLogin, nil
+//}
--- results.go
+++ results.go
@@ -2,7 +2,7 @@ package main
 
 // PrivateBody contains the response body to a private API call.
 type PrivateBody struct {
-	// Error   []string       `json:"error"`
+	//Error   PrivateError     `json:"error"`
 	Results PrivateResults `json:"results"`
 }
 
@@ -13,12 +13,16 @@ type PrivateResults struct {
 	CheckForm      string      `json:"checkForm"`
 }
 
-// PrivateUser contains information about the Deezer user retrieved from the
-// private API
+// PrivateUser contains information about the Deezer user retrieved from the private API.
 type PrivateUser struct {
 	ID          int    `json:"USER_ID"`
 	SessionID   string `json:"SESSION_ID"`
 	UserToken   string `json:"USER_TOKEN"`
 	Country     string `json:"COUNTRY"`
 	PlayerToken string `json:"PLAYER_TOKEN"`
+}
+
+// PrivateError is a mapping of error messages.
+type PrivateError struct {
+	GatewayError string `json:"GATEWAY_ERROR"`
 }
